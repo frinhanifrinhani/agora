9d924bb277d78d8c01a3184a50e45f7431bf7d8f
Frinhani <thiago.frinhani@ths.inf.br>
2024-06-07 08:45:12 -0300

    Instalando css e js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,3 @@
-<!-- resources/views/layouts/app.blade.php -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,14 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Àgora</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/Montserrat-fonts.css') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @stack('styles')
 </head>
 <body>
     <header>
-        <!-- Conteúdo do cabeçalho -->
         @include('partials.header')
     </header>
 
@@ -22,12 +20,10 @@
     </main>
 
     <footer>
-        <!-- Conteúdo do rodapé -->
         @include('partials.footer')
     </footer>
 
-    <!-- Adicione seus arquivos JavaScript aqui -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
     @stack('scripts')
 </body>
 </html>
