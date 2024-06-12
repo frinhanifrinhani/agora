072893a6f33a692ef409b63bc5788aad6a02cf16
Thiago Frinhani <thiago.frinhani@ths.inf.br>
2024-06-12 20:28:54 -0300

    Mensagens traduzidas em news

---
+++ config/global.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'PARAM_TITLE_REQUIRED' => 'O campo Titulo é obrigatório.',
+    'PARAM_TITLE_MAX' => 'O campo Titulo não deve ter mais de 255 caracteres.',
+    'PARAM_NEWS_REQUIRED' => 'O campo Notícia é obrigatório.',
+];
