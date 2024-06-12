cb5ee165c9a486ff6d232634c5f5204d82f46711
Alylson Monteiro <alylsonmonteiro@lampp-it.com.br>
2024-06-12 10:22:41 -0300

    Criação da página de notícias

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Àgora</title>
+    <title>Ágora</title>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/Montserrat-fonts.css') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
---
+++ resources/views/noticias/index.blade.php
@@ -0,0 +1,293 @@
+@extends('layouts.app')
+
+@section('title', 'Notícias')
+
+@section('content')
+<div>
+    
+    <div class="row justify-content-center">
+        <div class="col-md-8 align-items-center">
+            <div class="p-4 text-center">
+                <p class="h2">Notícias</p>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-10 pb-5">
+        
+            <div class="col-md-12 d-flex justify-content-center align-items-start">
+                <div class="p-4">
+                    <div class="card" style="width: 28rem;">
+                    <img src="http://localhost:8000/images/nt-ideia-produto.jpg" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <a href="https://agora.fiocruz.br/blog/event/planejamento-urbano-e-seguranca-hidrica/"
+                                    target="_blank" rel="noopener noreferrer"
+                                    style="color: #222222;
+                                        text-decoration: none;
+                                        font-size: 19px;
+                                        line-height: 1.4;
+                                        font-weight: 700;">
+                                    <strong>Planejamento Urbano e Segurança Hídrica</strong>
+                                </a>
+                            </h5>
+                            <p class="card-text" style="font-size: 12px; line-height: 1.42857143;">
+                                O anúncio dos 15 projetos ganhadores do Prêmio Transformação Digital na Saúde 2023 e das 10 equipes
+                                classificadas para a próxima etapa da Hackatona SUS Digital marcou o encerramento da 6ª Feira de
+                                Soluções para a Saúde, realizada pela primeira vez em Brasília (DF), de 27 a 29 de novembro,
+                                no Millennium Convention Center. O...
+                            </p>
+                            <div>
+                                <i class="fas fa-calendar-alt pb-3" style="color: #48773E;"></i><small> 30/11/2018</small>
+                                <small><i class="fas fa-comment" style="color: #48773E;"></i></i> 0 Comentário</small>
+                                <a class="btn btn-sm float-end"
+                                    style="border-radius: 20px; color: #48773E; background-color: #48773E;" href="#">
+                                    <small class="text-white">MAIS</small>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="p-4">
+                    <div class="card" style="width: 28rem;">
+                        <img src="http://localhost:8000/images/nt-vigilancia.jpg" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                            <a href="https://agora.fiocruz.br/blog/event/experiencias-de-ciencia-cidada-para-biodiversidade-e-saude/"
+                                target="_blank" rel="noopener noreferrer"
+                                style="color: #222222;
+                                    text-decoration: none;
+                                    font-size: 19px;
+                                    line-height: 1.4;
+                                    font-weight: 700;">
+                                <strong>Da ideia ao produto, Feira fomenta soluções em saúde digital</strong>
+                            </a>
+                            </h5>
+                            <p class="card-text" style="font-size: 12px; line-height: 1.42857143;">
+                                O anúncio dos 15 projetos ganhadores do Prêmio Transformação Digital na Saúde 2023 e das 10 equipes
+                                classificadas para a próxima etapa da Hackatona SUS Digital marcou o encerramento da 6ª Feira de
+                                Soluções para a Saúde, realizada pela primeira vez em Brasília (DF), de 27 a 29 de novembro,
+                                no Millennium Convention Center. O...
+                            </p>
+                            <div>
+                                <i class="fas fa-calendar-alt pb-3" style="color: #48773E;"></i><small> 30/11/2018</small>
+                                <small><i class="fas fa-comment" style="color: #48773E;"></i></i> 0 Comentário</small>
+                                <a class="btn btn-sm float-end"
+                                    style="border-radius: 20px; color: #48773E; background-color: #48773E;" href="#">
+                                    <small class="text-white">MAIS</small>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-12 d-flex justify-content-center align-items-start">
+                <div class="p-4">
+                    <div class="card" style="width: 28rem;">
+                    <img src="http://localhost:8000/images/nt-ideia-produto.jpg" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <a href="https://agora.fiocruz.br/blog/event/planejamento-urbano-e-seguranca-hidrica/"
+                                    target="_blank" rel="noopener noreferrer"
+                                    style="color: #222222;
+                                        text-decoration: none;
+                                        font-size: 19px;
+                                        line-height: 1.4;
+                                        font-weight: 700;">
+                                    <strong>Planejamento Urbano e Segurança Hídrica</strong>
+                                </a>
+                            </h5>
+                            <p class="card-text" style="font-size: 12px; line-height: 1.42857143;">
+                                O anúncio dos 15 projetos ganhadores do Prêmio Transformação Digital na Saúde 2023 e das 10 equipes
+                                classificadas para a próxima etapa da Hackatona SUS Digital marcou o encerramento da 6ª Feira de
+                                Soluções para a Saúde, realizada pela primeira vez em Brasília (DF), de 27 a 29 de novembro,
+                                no Millennium Convention Center. O...
+                            </p>
+                            <div>
+                                <i class="fas fa-calendar-alt pb-3" style="color: #48773E;"></i><small> 30/11/2018</small>
+                                <small><i class="fas fa-comment" style="color: #48773E;"></i></i> 0 Comentário</small>
+                                <a class="btn btn-sm float-end"
+                                    style="border-radius: 20px; color: #48773E; background-color: #48773E;" href="#">
+                                    <small class="text-white">MAIS</small>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="p-4">
+                    <div class="card" style="width: 28rem;">
+                        <img src="http://localhost:8000/images/nt-vigilancia.jpg" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                            <a href="https://agora.fiocruz.br/blog/event/experiencias-de-ciencia-cidada-para-biodiversidade-e-saude/"
+                                target="_blank" rel="noopener noreferrer"
+                                style="color: #222222;
+                                    text-decoration: none;
+                                    font-size: 19px;
+                                    line-height: 1.4;
+                                    font-weight: 700;">
+                                <strong>Da ideia ao produto, Feira fomenta soluções em saúde digital</strong>
+                            </a>
+                            </h5>
+                            <p class="card-text" style="font-size: 12px; line-height: 1.42857143;">
+                                O anúncio dos 15 projetos ganhadores do Prêmio Transformação Digital na Saúde 2023 e das 10 equipes
+                                classificadas para a próxima etapa da Hackatona SUS Digital marcou o encerramento da 6ª Feira de
+                                Soluções para a Saúde, realizada pela primeira vez em Brasília (DF), de 27 a 29 de novembro,
+                                no Millennium Convention Center. O...
+                            </p>
+                            <div>
+                                <i class="fas fa-calendar-alt pb-3" style="color: #48773E;"></i><small> 30/11/2018</small>
+                                <small><i class="fas fa-comment" style="color: #48773E;"></i></i> 0 Comentário</small>
+                                <a class="btn btn-sm float-end"
+                                    style="border-radius: 20px; color: #48773E; background-color: #48773E;" href="#">
+                                    <small class="text-white">MAIS</small>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-12 d-flex justify-content-center align-items-start">
+                <div class="p-4">
+                    <div class="card" style="width: 28rem;">
+                    <img src="http://localhost:8000/images/nt-ideia-produto.jpg" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <a href="https://agora.fiocruz.br/blog/event/planejamento-urbano-e-seguranca-hidrica/"
+                                    target="_blank" rel="noopener noreferrer"
+                                    style="color: #222222;
+                                        text-decoration: none;
+                                        font-size: 19px;
+                                        line-height: 1.4;
+                                        font-weight: 700;">
+                                    <strong>Planejamento Urbano e Segurança Hídrica</strong>
+                                </a>
+                            </h5>
+                            <p class="card-text" style="font-size: 12px; line-height: 1.42857143;">
+                                O anúncio dos 15 projetos ganhadores do Prêmio Transformação Digital na Saúde 2023 e das 10 equipes
+                                classificadas para a próxima etapa da Hackatona SUS Digital marcou o encerramento da 6ª Feira de
+                                Soluções para a Saúde, realizada pela primeira vez em Brasília (DF), de 27 a 29 de novembro,
+                                no Millennium Convention Center. O...
+                            </p>
+                            <div>
+                                <i class="fas fa-calendar-alt pb-3" style="color: #48773E;"></i><small> 30/11/2018</small>
+                                <small><i class="fas fa-comment" style="color: #48773E;"></i></i> 0 Comentário</small>
+                                <a class="btn btn-sm float-end"
+                                    style="border-radius: 20px; color: #48773E; background-color: #48773E;" href="#">
+                                    <small class="text-white">MAIS</small>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="p-4">
+                    <div class="card" style="width: 28rem;">
+                        <img src="http://localhost:8000/images/nt-vigilancia.jpg" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                            <a href="https://agora.fiocruz.br/blog/event/experiencias-de-ciencia-cidada-para-biodiversidade-e-saude/"
+                                target="_blank" rel="noopener noreferrer"
+                                style="color: #222222;
+                                    text-decoration: none;
+                                    font-size: 19px;
+                                    line-height: 1.4;
+                                    font-weight: 700;">
+                                <strong>Da ideia ao produto, Feira fomenta soluções em saúde digital</strong>
+                            </a>
+                            </h5>
+                            <p class="card-text" style="font-size: 12px; line-height: 1.42857143;">
+                                O anúncio dos 15 projetos ganhadores do Prêmio Transformação Digital na Saúde 2023 e das 10 equipes
+                                classificadas para a próxima etapa da Hackatona SUS Digital marcou o encerramento da 6ª Feira de
+                                Soluções para a Saúde, realizada pela primeira vez em Brasília (DF), de 27 a 29 de novembro,
+                                no Millennium Convention Center. O...
+                            </p>
+                            <div>
+                                <i class="fas fa-calendar-alt pb-3" style="color: #48773E;"></i><small> 30/11/2018</small>
+                                <small><i class="fas fa-comment" style="color: #48773E;"></i></i> 0 Comentário</small>
+                                <a class="btn btn-sm float-end"
+                                    style="border-radius: 20px; color: #48773E; background-color: #48773E;" href="#">
+                                    <small class="text-white">MAIS</small>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-12 d-flex justify-content-center align-items-start">
+                <div class="p-4">
+                    <div class="card" style="width: 28rem;">
+                    <img src="http://localhost:8000/images/nt-ideia-produto.jpg" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <a href="https://agora.fiocruz.br/blog/event/planejamento-urbano-e-seguranca-hidrica/"
+                                    target="_blank" rel="noopener noreferrer"
+                                    style="color: #222222;
+                                        text-decoration: none;
+                                        font-size: 19px;
+                                        line-height: 1.4;
+                                        font-weight: 700;">
+                                    <strong>Planejamento Urbano e Segurança Hídrica</strong>
+                                </a>
+                            </h5>
+                            <p class="card-text" style="font-size: 12px; line-height: 1.42857143;">
+                                O anúncio dos 15 projetos ganhadores do Prêmio Transformação Digital na Saúde 2023 e das 10 equipes
+                                classificadas para a próxima etapa da Hackatona SUS Digital marcou o encerramento da 6ª Feira de
+                                Soluções para a Saúde, realizada pela primeira vez em Brasília (DF), de 27 a 29 de novembro,
+                                no Millennium Convention Center. O...
+                            </p>
+                            <div>
+                                <i class="fas fa-calendar-alt pb-3" style="color: #48773E;"></i><small> 30/11/2018</small>
+                                <small><i class="fas fa-comment" style="color: #48773E;"></i></i> 0 Comentário</small>
+                                <a class="btn btn-sm float-end"
+                                    style="border-radius: 20px; color: #48773E; background-color: #48773E;" href="#">
+                                    <small class="text-white">MAIS</small>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="p-4">
+                    <div class="card" style="width: 28rem;">
+                        <img src="http://localhost:8000/images/nt-vigilancia.jpg" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                            <a href="https://agora.fiocruz.br/blog/event/experiencias-de-ciencia-cidada-para-biodiversidade-e-saude/"
+                                target="_blank" rel="noopener noreferrer"
+                                style="color: #222222;
+                                    text-decoration: none;
+                                    font-size: 19px;
+                                    line-height: 1.4;
+                                    font-weight: 700;">
+                                <strong>Da ideia ao produto, Feira fomenta soluções em saúde digital</strong>
+                            </a>
+                            </h5>
+                            <p class="card-text" style="font-size: 12px; line-height: 1.42857143;">
+                                O anúncio dos 15 projetos ganhadores do Prêmio Transformação Digital na Saúde 2023 e das 10 equipes
+                                classificadas para a próxima etapa da Hackatona SUS Digital marcou o encerramento da 6ª Feira de
+                                Soluções para a Saúde, realizada pela primeira vez em Brasília (DF), de 27 a 29 de novembro,
+                                no Millennium Convention Center. O...
+                            </p>
+                            <div>
+                                <i class="fas fa-calendar-alt pb-3" style="color: #48773E;"></i><small> 30/11/2018</small>
+                                <small><i class="fas fa-comment" style="color: #48773E;"></i></i> 0 Comentário</small>
+                                <a class="btn btn-sm float-end"
+                                    style="border-radius: 20px; color: #48773E; background-color: #48773E;" href="#">
+                                    <small class="text-white">MAIS</small>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+@endsection
