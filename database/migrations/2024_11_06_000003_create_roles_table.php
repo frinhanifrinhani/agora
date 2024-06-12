ad85e1e947d08aa465b540a546d897db15a581cc
Frinhani <thiago.frinhani@ths.inf.br>
2024-06-12 16:52:38 -0300

    Refatoração cod linguagem

--- database/migrations/2024_11_06_000003_create_roles_table.php
+++ database/migrations/2024_11_06_000003_create_roles_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('nome');
-            $table->string('descricao')->nullable();
+            $table->string('name');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
