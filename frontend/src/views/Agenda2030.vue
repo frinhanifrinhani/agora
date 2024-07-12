396b36317920a67dc4b26a0efb4982784917ec4d
barbarabea <barbarabeatrizsilva6@gmail.com>
2024-07-12 11:48:19 -0300

    Ajustes no estilo da página Agenda 2030

--- frontend/src/views/Agenda2030.vue
+++ frontend/src/views/Agenda2030.vue
@@ -16,59 +16,77 @@ export default {
 </script>
 
 <style>
- button.btn{
-  border-radius: 20px;
- }
+  button.btn{
+    border-radius: 20px;
+  }
 
-h2.green-title {
-  color: #60BF6E;
-}
+  h2.green-title {
+    color: #60BF6E;
+  }
 
-span.green-title {
-  color: #60BF6E;
-}
+  span.green-title {
+    color: #60BF6E;
+  }
 
-h1.green-title {
-  color: #60BF6E;
+  h1.green-title {
+    color: #60BF6E;
 
-}
+  }
 
-.flip-card {
-  background-color: transparent;
-  width: 300px;
-  height: 200px;
-  /* perspective: 1000px;  */
-}
+  .flip-card {
+    background-color: transparent;
+    width: 300px;
+    height: 200px;
+    margin-bottom: 100px;
+    /* perspective: 1000px;  */
+  }
 
-.flip-card-inner {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  text-align: center;
-  transition: transform 0.6s ease-in-out;
-  transform-style: preserve-3d;
-}
+  .flip-card-inner {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    transition: transform 0.6s ease-in-out;
+    transform-style: preserve-3d;
+  }
 
-.flip-card:hover .flip-card-inner {
-  transform: rotateY(180deg);
-}
+  .flip-card:hover .flip-card-inner {
+    transform: rotateY(180deg);
+  }
 
-.flip-card-front, .flip-card-back {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  -webkit-backface-visibility: hidden; /* Safari */
-  backface-visibility: hidden;
-}
+  .flip-card-front, .flip-card-back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    -webkit-backface-visibility: hidden; /* Safari */
+    backface-visibility: hidden;
+  }
 
-.flip-card-front {
-  background-color: #bbb;
-  color: black;
-}
+  .flip-card-front {
+    background-color: #bbb;
+    color: black;
+  }
 
-.flip-card-back {
-  color: gray;
-  transform: rotateY(180deg);
-  padding: 2em;
-}
+  .flip-card-back {
+    color: gray;
+    transform: rotateY(180deg);
+    padding: 2em;
+  }
+
+  @media (max-width: 1112px) {
+      .image-responsive{
+        display: none;
+      }
+
+      div.text-responsive {
+        width: 90%; /* equivalente a col-md-10 width */
+      } 
+
+      div.card-responsive{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+      
+      }
+  } 
 </style>
