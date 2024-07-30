10a88c1bb2ef5661a3f9f4d390bd9e6722bb747f
barbarabea <barbarabeatrizsilva6@gmail.com>
2024-07-30 16:30:15 -0300

    Conteúdo página evento-resumo

---
+++ frontend/src/views/event/EventOverview.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="content">
+        <EventOverviewComponent />
+    </div>
+</template>
+
+<script>
+  import EventOverviewComponent from "../../components/event/EventOverview.vue";
+  
+  export default {
+    name: "EventOverview",
+    components: {
+        EventOverviewComponent,
+    },
+  };
+  </script>
