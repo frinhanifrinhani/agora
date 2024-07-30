1e795ad5a31baa9af73a8dd531041c265b3a2d83
barbarabea <barbarabeatrizsilva6@gmail.com>
2024-07-30 16:30:15 -0300

    Conteúdo página evento-resumo

---
+++ frontend/src/components/event/EventOverview.vue
@@ -0,0 +1,143 @@
+<template>
+    <div>
+        <div class="row justify-content-center">
+            <section class="col-md-10">
+                <h1>
+                    Em evento na ONU sobre a Agenda 2030, Fiocruz apresenta suas ações na pandemia
+                </h1>
+
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="#">Ágora Fiocruz</a></li>
+                        <li class="breadcrumb-item"><a href="#">2030 Agenda</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Em evento na ONU sobre a Agenda 2030, Fiocruz apresenta suas ações na pandemia</li>
+                    </ol>
+                </nav>
+            </section>
+        </div>
+
+        <section class="row">
+            <section class="col-sm-8">
+                <img src="@/assets/images/estrategia-fiocruz-para-a-agenda-2030.png" alt="Imagem com o texto soluções estratégicas para a agenda 2030">
+            </section>
+
+            <section>
+                <h3>Agência Fiocruz de Notícias</h3>
+
+                <p>
+                    A Fiocruz co-organizou e participou, nesta terça-feira (18/7), de dois eventos paralelos sobre implementação da Agenda 2030 no Fórum Político de Alto Nível da ONU sobre Desenvolvimento Sustentável, em Nova York. Segundo os painelistas da mesa da manhã, um dos trunfos dos eventos foi a união, no debate, da sociedade civil e do governo, um símbolo de como o tema da Agenda 2030 deve ser tratado. Além de destacarem a necessidade de fortalecer a implementação dos Objetivos de Desenvolvimento Sustentável (ODS), as falas trataram de como o Brasil está se reestruturando para retomar essa Agenda e as medidas para acelerar e atingir as metas de desenvolvimento.
+                </p>
+
+            
+
+                <img src="" alt="imagem ">
+                <p>descrição da imagem</p>
+                
+                <p>
+                    Segundo o representante permanente do Brasil na ONU, Sergio França Danese, apenas 20% dos objetivos estão no ‘caminho certo’. Ele destacou que ‘as lacunas de implementação são imensas, além de o mundo ter retrocedido em diversos objetivos, com crescimento da pobreza’ (Foto: reprodução da internet)
+                </p>
+
+                <p>
+                    A mediação do evento da manhã foi do coordenador-geral de Desenvolvimento Sustentável do Ministério das Relações Exteriores, Vicente Araujo, que afirmou que a implementação dos ODS não é possível sem o engajamento dos setores privado, da sociedade civil e da academia: “Todos esses setores da nossa sociedade precisam estar engajados, se quisermos implementar com sucesso a Agenda 2030”, disse Araujo. 
+                </p>
+
+                <p>
+                    Representante permanente do Brasil na ONU, Sergio França Danese também pontuou a importância de um esforço conjunto, com a sociedade civil, para implementação dos ODS: “Como um plano de ação para as pessoas, o planeta e a prosperidade, a Agenda 2030 não foi concebida como um documento voltado ou limitado por burocratas”, salientou ele, enfatizando que “os objetivos da Agenda exigem um trabalho conjunto de todos e em todos os níveis”. 
+                </p>
+
+                <p>
+                    O embaixador citou o relatório da ONU que aponta preocupação com relação aos ODS que diz que eles “estão desaparecendo no vidro traseiro”. Ainda segundo o relatório, como citou o embaixador, globalmente, apenas 20% dos objetivos estão no “caminho certo”. Ele destacou que “as lacunas de implementação são imensas, além de o mundo ter retrocedido em diversos objetivos, com crescimento da pobreza, da fome e da insegurança alimentar, com os países em desenvolvimento sendo os mais atingidos pelos impactos das múltiplas crises enfrentadas atualmente”. Danse enfatizou que, “uma situação tão alarmante” como a atual, requer, mais do que nunca, união de esforços, cooperações e parcerias, “uma ação coletiva para fortalecer e acelerar a implementação dos ODS”. 
+                </p>
+
+                <p>
+                    O  coordenador da Estratégia Fiocruz para a Agenda 2030 (EFA 2030), Paulo Gadelha, participou da mesa, debatendo sobre como a pandemia dificultou a conquista dos ODS e os aprendizados que ficaram com a Covid-19. Gadelha também enumerou ações da Fiocruz no enfrentamento da crise, como a Fundação ter aumentado significativamente a capacidade de testagem de Covid-19 no país, a construção do hospital permanente para doenças infecciosas do Instituto Nacional de Infectologia (INI/Fiocruz) e a conclusão da transferência de tecnologia que resultou na produção 100% nacional da vacina para Covid-19. 
+                </p>
+
+                <img src="" alt="segunda imagem">
+
+                <p>
+                    O  coordenador da Estratégia Fiocruz para a Agenda 2030 (EFA 2030), Paulo Gadelha, participou do evento e apresentou as ações da Fundação (Foto: reprodução da internet) 
+                </p>
+
+                <p>
+                    Gadelha afirmou ser fundamental disseminar o conhecimento e internalizar os valores, objetivos e instrumentos de implementação da Agenda 2030. Além disso, ele destacou que se deve priorizar o enfrentamento das desigualdades sociais e o apoio a programas de recuperação econômica e proteção social, além de garantir políticas e financiamento para os ODS. 
+                </p>
+
+                <p>
+                    “O Brasil, sob a liderança do presidente Lula, demonstra um compromisso renovado com a Agenda 2030, enfatizando a equidade e a solidariedade para os mais vulneráveis, com o objetivo de ‘não deixar ninguém para trás’. Na Paris Summit for Global Finance, ele disse, em alto e bom som: ‘(…) Já temos uma bússola acordada multilateralmente: a Agenda 2030’”, concluiu Gadelha.
+                </p>
+            </section>
+
+            <section>
+                <h3>Reconstrução</h3>
+
+                <p>
+                    Representando o Secretária-Geral da Presidência da República, o assessor especial Flávio Schuch ressaltou que a pasta retoma um papel que era seu no passado, o de permitir e garantir a participação da sociedade civil no governo e, por isso, está reconstruindo espaços. “Neste processo de reconstrução, queremos que o tecido social da participação política das organizações da sociedade civil sejam fortalecidos”, destacou.
+                </p>
+
+                <p>
+                    Do Governo Federal participaram também a secretária de Povos e Comunidades Tradicionais e Desenvolvimento Rural Sustentável, Edel Moraes, que falou sobre o desenvolvimento sustentável nas cidades amazônicas; a subsecretária de Desenvolvimento Econômico Sustentável, Cristina Reis, que comentou sobre o Plano de Transformação Ecológica; e o embaixador Antonio da Costa e Silva, assessor especial do Ministério das Cidades. Entre os representantes da sociedade civil estavam o diretor-presidente do Instituto Cidades Sustentáveis, Jorge Abrahão, que apresentou um instrumento de monitoramento dos ODS nas cidades brasileiras, e a co-fundadora e coordenadora-geral da Gestos e co-facilitadora do GT Agenda 2030, Alessandra Nilo, que falou sobre como o Spotlight Report pode ser um  instrumento de monitoramento da Agenda 2030 pela sociedade civil. 
+                </p>
+            </section>
+
+            <section>
+                <h3>Experiência da América Latina</h3>
+
+                <p>
+                    Às 19h30, ocorreu o segundo evento paralelo organizado pelo governo e com co-realização da Fiocruz. No evento da noite, o foco se virou para a experiência da América Latina na implementação da Agenda 2030, reunindo representantes governamentais de países da região. O pesquisador da Fiocruz Romulo Paes de Souza representou o Brasil no debate.
+                </p>
+            </section>
+
+            <div>
+                <i class="fa-solid fa-calendar-days"></i>
+                <span class="ms-2">20/07/2023</span>
+            </div>
+
+            <div>
+                <i class="fa-solid fa-comment"></i>
+                <span>Comments</span>
+            </div>
+
+            <div>
+                <span>Compartilhar</span>
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-solid fa-envelope"></i>
+                <i class="fa-brands fa-linkedin"></i>
+            </div>
+        </section>
+
+        <section>
+           <nav class="navbar navbar-light bg-light">
+                <form class="form-inline">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Buscar">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">BUSCAR</button>
+                </form>
+            </nav>
+
+            <section>
+                <div>
+                    <h3>POSTS RECENTES</h3>
+
+                    <a href="#" class="btn btn-secondary btn-lg disabled" role="button" aria-disabled="true">
+                        Da ideia ao produto, Feira fomenta soluções em saúde digital
+                    </a>
+                </div>
+
+                <div>
+                    <h3>TAGS</h3>
+
+                    <button type="button" class="btn btn-outline-success">Success</button>
+                </div>
+            </section>
+        </section>
+
+        
+    </div>
+</template>
+
+<script>
+    export default {
+    name: "EventOverview",
+    };
+</script>
