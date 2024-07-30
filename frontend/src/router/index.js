cefabfd0c73140512cee45dc30c52d69dc1c2d90
barbarabea <barbarabeatrizsilva6@gmail.com>
2024-07-30 16:30:15 -0300

    Conteúdo página evento-resumo

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../views/Home.vue'
 import News from '../views/news/News.vue'
 import Event from '../views/event/Event.vue'
 import EventPage from '../views/event/EventPage.vue'
+import EventOverview from '../views/event/EventOverview.vue'
 import About from '../views/About.vue'
 import Agenda2030 from '../views/Agenda2030.vue'
 
@@ -12,7 +13,9 @@ const routes = [
   { path: '/evento', name: 'Event', component: Event },
   { path: '/pagina-evento', name: 'EventPage', component: EventPage },
   { path: '/sobre', name: 'About', component: About },
-  { path: '/agenda-2030', name: 'Agenda2030', component: Agenda2030},
+  { path: '/agenda-2030', name: 'Agenda2030', component: Agenda2030}, 
+  { path: '/evento-resumo', name: 'EventOverview', component: EventOverview},
+
 ];
 
 const router = createRouter({
