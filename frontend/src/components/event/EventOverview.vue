2bfc5ebc170c51e3f196761fad36281f30574b9a
barbarabea <barbarabeatrizsilva6@gmail.com>
2024-08-01 15:41:15 -0300

    Adicionando estilo da página

--- frontend/src/components/event/EventOverview.vue
+++ frontend/src/components/event/EventOverview.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
         <div class="row justify-content-center">
+
+            <!--Seção breadcrumbs-->
+            <img src="@/assets/images/breadcrumbs-bg.jpg" class="img-fluid" alt="Imagem de fundo breadcrumbs">
+
             <section class="col-md-10">
                 <h1>
                     Em evento na ONU sobre a Agenda 2030, Fiocruz apresenta suas ações na pandemia
@@ -16,23 +20,26 @@
             </section>
         </div>
 
-        <section class="row">
-            <section class="col-sm-8">
-                <img src="@/assets/images/estrategia-fiocruz-para-a-agenda-2030.png" alt="Imagem com o texto soluções estratégicas para a agenda 2030">
+        <!--Seção notícia-->
+        <section class="row justify-content-center">
+            <section class="col-md-10">
+                <img src="@/assets/images/estrategia-fiocruz-para-a-agenda-2030.png" class="w-75" alt="Imagem com o texto soluções estratégicas para a agenda 2030" >
             </section>
 
-            <section>
-                <h3>Agência Fiocruz de Notícias</h3>
+            <section class="col-md-10">
+                <h3 class="mt-5 color-green">Agência Fiocruz de Notícias</h3>
 
                 <p>
                     A Fiocruz co-organizou e participou, nesta terça-feira (18/7), de dois eventos paralelos sobre implementação da Agenda 2030 no Fórum Político de Alto Nível da ONU sobre Desenvolvimento Sustentável, em Nova York. Segundo os painelistas da mesa da manhã, um dos trunfos dos eventos foi a união, no debate, da sociedade civil e do governo, um símbolo de como o tema da Agenda 2030 deve ser tratado. Além de destacarem a necessidade de fortalecer a implementação dos Objetivos de Desenvolvimento Sustentável (ODS), as falas trataram de como o Brasil está se reestruturando para retomar essa Agenda e as medidas para acelerar e atingir as metas de desenvolvimento.
                 </p>
 
-            
-
-                <img src="" alt="imagem ">
-                <p>descrição da imagem</p>
-                
+                <figure  class="figure">
+                    <img src="@/assets/images/onudentro.jpg" alt="imagem ">
+                    <figcaption class="figure-caption">
+                        Segundo o representante permanente do Brasil na ONU, Sergio França Danese, apenas 20% dos objetivos estão no ‘caminho certo’. Ele destacou que ‘as lacunas de implementação são imensas, além de o mundo ter retrocedido em diversos objetivos, com crescimento da pobreza’ (Foto: reprodução da internet)
+                    </figcaption>
+                </figure>
+                                
                 <p>
                     Segundo o representante permanente do Brasil na ONU, Sergio França Danese, apenas 20% dos objetivos estão no ‘caminho certo’. Ele destacou que ‘as lacunas de implementação são imensas, além de o mundo ter retrocedido em diversos objetivos, com crescimento da pobreza’ (Foto: reprodução da internet)
                 </p>
@@ -53,11 +60,12 @@
                     O  coordenador da Estratégia Fiocruz para a Agenda 2030 (EFA 2030), Paulo Gadelha, participou da mesa, debatendo sobre como a pandemia dificultou a conquista dos ODS e os aprendizados que ficaram com a Covid-19. Gadelha também enumerou ações da Fiocruz no enfrentamento da crise, como a Fundação ter aumentado significativamente a capacidade de testagem de Covid-19 no país, a construção do hospital permanente para doenças infecciosas do Instituto Nacional de Infectologia (INI/Fiocruz) e a conclusão da transferência de tecnologia que resultou na produção 100% nacional da vacina para Covid-19. 
                 </p>
 
-                <img src="" alt="segunda imagem">
-
-                <p>
-                    O  coordenador da Estratégia Fiocruz para a Agenda 2030 (EFA 2030), Paulo Gadelha, participou do evento e apresentou as ações da Fundação (Foto: reprodução da internet) 
-                </p>
+                <figure class="figure">
+                    <img src="@/assets/images/onugadelha.jpg" alt="segunda imagem">
+                    <figcaption class="figure-caption">
+                        O coordenador da Estratégia Fiocruz para a Agenda 2030 (EFA 2030), Paulo Gadelha, participou do evento e apresentou as ações da Fundação (Foto: reprodução da internet) 
+                    </figcaption>
+                </figure>
 
                 <p>
                     Gadelha afirmou ser fundamental disseminar o conhecimento e internalizar os valores, objetivos e instrumentos de implementação da Agenda 2030. Além disso, ele destacou que se deve priorizar o enfrentamento das desigualdades sociais e o apoio a programas de recuperação econômica e proteção social, além de garantir políticas e financiamento para os ODS. 
@@ -68,8 +76,8 @@
                 </p>
             </section>
 
-            <section>
-                <h3>Reconstrução</h3>
+            <section class="col-md-10">
+                <h4 class="mt-4 color-green">Reconstrução</h4>
 
                 <p>
                     Representando o Secretária-Geral da Presidência da República, o assessor especial Flávio Schuch ressaltou que a pasta retoma um papel que era seu no passado, o de permitir e garantir a participação da sociedade civil no governo e, por isso, está reconstruindo espaços. “Neste processo de reconstrução, queremos que o tecido social da participação política das organizações da sociedade civil sejam fortalecidos”, destacou.
@@ -80,64 +88,74 @@
                 </p>
             </section>
 
-            <section>
-                <h3>Experiência da América Latina</h3>
+            <section class="col-md-10">
+                <h4 class="mt-4 color-green">Experiência da América Latina</h4>
 
                 <p>
                     Às 19h30, ocorreu o segundo evento paralelo organizado pelo governo e com co-realização da Fiocruz. No evento da noite, o foco se virou para a experiência da América Latina na implementação da Agenda 2030, reunindo representantes governamentais de países da região. O pesquisador da Fiocruz Romulo Paes de Souza representou o Brasil no debate.
                 </p>
             </section>
-
-            <div>
-                <i class="fa-solid fa-calendar-days"></i>
-                <span class="ms-2">20/07/2023</span>
-            </div>
-
-            <div>
-                <i class="fa-solid fa-comment"></i>
-                <span>Comments</span>
-            </div>
-
-            <div>
-                <span>Compartilhar</span>
-                <i class="fa-brands fa-facebook"></i>
-                <i class="fa-brands fa-instagram"></i>
-                <i class="fa-solid fa-envelope"></i>
-                <i class="fa-brands fa-linkedin"></i>
-            </div>
         </section>
 
-        <section>
-           <nav class="navbar navbar-light bg-light">
-                <form class="form-inline">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Buscar">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">BUSCAR</button>
-                </form>
-            </nav>
-
-            <section>
-                <div>
-                    <h3>POSTS RECENTES</h3>
-
-                    <a href="#" class="btn btn-secondary btn-lg disabled" role="button" aria-disabled="true">
-                        Da ideia ao produto, Feira fomenta soluções em saúde digital
-                    </a>
+        <!--Seção entre em contato-->
+        <section class="row justify-content-center">
+            <section class="col-md-10">
+                <div class="mb-3">
+                    <i class="fa-solid fa-calendar-days color-green"></i>
+                    <span class="ms-2 color-grey">20/07/2023</span>
                 </div>
 
-                <div>
-                    <h3>TAGS</h3>
+                <!-- <div>
+                    <i class="fa-solid fa-comment"></i>
+                    <span>Comments</span>
+                </div> -->
 
-                    <button type="button" class="btn btn-outline-success">Success</button>
+                <div>
+                    <span class="color-grey">Compartilhar</span>
+                    <i class="fa-brands fa-facebook icons-share color-green"></i>
+                    <i class="fa-brands fa-instagram icons-share color-green"></i>
+                    <i class="fa-solid fa-envelope icons-share color-green"></i>
+                    <i class="fa-brands fa-linkedin icons-share color-green"></i>
                 </div>
             </section>
         </section>
 
-        
+        <!--Seção buscar e tags-->
+        <section>
+           <nav class="navbar navbar-light bg-light">
+                <form class="form-inline form-inline-equal">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Buscar">
+                    <button class="btn btn-outline-success btn-block" type="submit">BUSCAR</button>
+                </form>
+            </nav>
+
+            <section class="col-md-2">
+                <div>
+                    <h5 class="mt-5 color-green">POSTS RECENTES</h5>
+                    <hr/>
+                    <button type="button" class="custom-button-tags">
+                        <small>Da ideia ao produto, Feira fomenta soluções em saúde digital</small>
+                    </button>
+                </div>
+
+                <div>
+                    <h5 class="mt-5 color-green">TAGS</h5>
+                    <hr/>
+                    <button type="button" class="custom-button-tags">
+                        TAGS
+                    </button>
+                </div>
+            </section>
+        </section> 
     </div>
 </template>
 
 <script>
+import HorizontalLine from '../icons/HorizontalLine.vue';
     export default {
     name: "EventOverview",
-    };
+    components: {
+        HorizontalLine,
+    }
+};
 </script>
