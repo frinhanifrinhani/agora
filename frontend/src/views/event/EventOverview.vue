650057897a7610d28c203a4c633a490f54010b3e
barbarabea <barbarabeatrizsilva6@gmail.com>
2024-08-01 15:41:15 -0300

    Adicionando estilo da página

--- frontend/src/views/event/EventOverview.vue
+++ frontend/src/views/event/EventOverview.vue
@@ -13,4 +13,40 @@
         EventOverviewComponent,
     },
   };
-  </script>
+</script>
+
+<style>
+  .color-green {
+    color: #55A649;
+  }
+
+  .color-grey {
+    color: #4F4F4F;
+  }
+
+  .icons-share {
+    font-size: 1.5em;
+    margin-left: 0.3em;
+  }
+
+  .custom-button-tags {
+      background-color: #E9E9E9; 
+      color: #6D6D6D; 
+      border: none; 
+      border-radius: 5px;
+      padding: 8px 16px; 
+      text-align: left; 
+      text-decoration: none; 
+      display: inline-block;
+      cursor: pointer;
+      transition: background-color 0.3s, border-bottom 0.3s; 
+  }
+
+  .custom-button-tags:hover {
+     color: #55A649;
+     text-decoration: underline;
+     text-decoration-color: #55A649;
+    }
+
+</style>
+
