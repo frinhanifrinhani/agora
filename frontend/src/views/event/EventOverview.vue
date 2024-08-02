ee69b434003c0779596dc5e02ebf2a8af1adf521
barbarabea <barbarabeatrizsilva6@gmail.com>
2024-08-02 10:08:04 -0300

    Adição de breadcrumb e botão buscar

--- frontend/src/views/event/EventOverview.vue
+++ frontend/src/views/event/EventOverview.vue
@@ -24,9 +24,38 @@
     color: #4F4F4F;
   }
 
+  .breadcrumbs-background {
+    position: relative;
+    width: 100%;
+    padding: 50px 0;
+    background: 
+    linear-gradient(rgba(0, 0, 0, 0.5) 0%, #000000 100%),
+    url('@/assets/images/breadcrumbs-bg.jpg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+
+  .breadcrumbs-background h1, 
+  .breadcrumb a, 
+  .breadcrumb .breadcrumb-item.active,
+  .breadcrumb .breadcrumb-item + .breadcrumb-item::before 
+  {
+    color: white !important;
+    text-decoration: none;
+  }
+
+  .breadcrumb-item:hover{
+    text-decoration: underline !important;
+  }
+
   .icons-share {
     font-size: 1.5em;
     margin-left: 0.3em;
+  }
+
+  .custom-button-border {
+    border-radius: 4px !important;
   }
 
   .custom-button-tags {
@@ -47,6 +76,24 @@
      text-decoration: underline;
      text-decoration-color: #55A649;
     }
+
+    .form-search{
+      display: flex;
+      flex-wrap: nowrap;
+      width: 100%;
+    }
+
+    .form-search {
+        display: block;
+        width: 100%;
+    }
+
+    .form-search .form-control,
+    .form-search .btn {
+        width: 100%;
+        margin-bottom: 10px; 
+    }
+
 
 </style>
 
