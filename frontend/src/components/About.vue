afb6c5cc8bb10c3bfab4f054a3dd9c794044c77a
barbarabea <barbarabeatrizsilva6@gmail.com>
2024-08-15 15:35:31 -0300

    Atualização do texto da página sobre

--- frontend/src/components/About.vue
+++ frontend/src/components/About.vue
@@ -11,96 +11,50 @@
     <div class="row justify-content-center">
       <div class="col-md-10">
         <p>
-          Educação, Ciência e Tecnologia abertas: que é isso? A Ágora vai te ajudar a entender esses conceitos e, principalmente, a fazer o que você já faz, só que sob esse novo paradigma. Pois esse é o objetivo da Ágora: discutir, disseminar e fomentar a adoção de uma ética do compartilhamento e participação cidadã na produção da Educação, Ciência e Tecnologia no Brasil.
+          A Ágora tem como objetivo facilitar a compreensão e aplicação de conceitos-chave dentro de um novo paradigma. Sua missão é promover a discussão, disseminação e adoção de uma ética baseada no compartilhamento e na participação cidadã, fundamentais para a produção colaborativa de conhecimento em Educação, Ciência e Tecnologia
         </p>
         <p>
-          Em nosso dia-a-dia, estamos acostumados a tomar contato com os resultados de pesquisas científicas por meio de artigos publicados em periódicos ou em capítulos de livros. Há limitações de espaço, de escopo, de forma e tantas outras que não nos permitem conhecer e acessar de fato como esse conhecimento foi produzido. Qual roteiro foi utilizado nas entrevistas realizadas e como foi desenvolvida a categorização das mesmas? Quais os resultados detalhados dos mapeamentos de DNA que levaram às conclusões do artigo? Como os vieses do estudo de Coorte foram controlados? Quais textos foram utilizados na revisão bibliográfica? Informações como essas ficam inacessíveis a outras pessoas que não aquelas diretamente envolvidas na produção da pesquisa.
+          No nosso cotidiano, estamos acostumados a acessar os resultados de pesquisas científicas por meio de artigos publicados em periódicos ou capítulos de livros. No entanto, há inúmeras limitações de espaço, de escopo e de forma que nos impedem de conhecer e acessar, de fato, o processo de produção desse conhecimento. Essas e outras informações muitas vezes ficam inacessíveis, restritas às pessoas diretamente envolvidas na pesquisa.
         </p>
         <p>
-          Na área da educação, o cenário não é tão diferente. Ao oferecer um curso, por exemplo, são produzidos vários instrumentos e materiais intermediários, tais como planos de aula, avaliações, apresentações, metodologias pedagógicas… mas assim como na ciência, esse conhecimento costuma ficar restrito aos envolvidos. Aos demais resta conhecer apenas a proposta geral do curso.
+          Na área da educação, a situação não é diferente. Ao desenvolver um curso, por exemplo, são produzidos diversos materiais e instrumentos intermediários, como planos de aula, avaliações, apresentações e metodologias pedagógicas. Contudo, assim como na ciência, esse conhecimento tende a permanecer restrito aos envolvidos, enquanto os demais só têm acesso à proposta geral do curso.
         </p>
         <p>
-          Capitaneada pela Fundação Oswaldo Cruz, a Ágora é uma iniciativa que visa responder a estas inquietações ao mesmo tempo que propõe-se ser um espaço de cooperação e troca de experiências em torno destas temáticas, voltada a empoderar os indivíduos para atuar na governança das políticas públicas e saúde.
+          É aqui que entra a Ágora, uma iniciativa liderada pela Fundação Oswaldo Cruz (Fiocruz), que busca responder a essas questões, ao mesmo tempo em que se propõe a ser um espaço de cooperação e troca de experiências nessas áreas. A Ágora visa capacitar os indivíduos para que possam participar da governança das políticas públicas e da saúde.
         </p>
-        
-        <blockquote class="blockquote text-center">
-          <i class="fas fa-quote-left"></i>
-          <p class="mb-5 small">
-            A consolidação dessa Ágora da saúde, espaço público no âmbito cibernético (Rede Social do SUS), constituirá uma estrutura de mediação discursiva entre a política e a sociedade possibilitando a convergência de mídias digitais, que favoreceria a comunicação generalizada. [..] Pensamos, portanto, que para o envolvimento de diferentes atores no processo de gestão estratégica, a Plataforma Ágora da Saúde, enquanto uma estrutura de apoio à governança para as redes sociais do SUS deva estimular o fluxo contínuo de informações estratégicas para orientar as ações desses atores, conforme os princípios, diretrizes e estratégias do SUS (MARTINS, 2013, p. 238)¹
-          </p>
-        </blockquote>
+      
+        <p>
+          A consolidação dessa Ágora da Saúde, como um espaço público no ambiente cibernético (Rede Social do SUS), criará uma estrutura de mediação discursiva entre a política e a sociedade, possibilitando a convergência de mídias digitais e favorecendo uma comunicação mais ampla e inclusiva. Conforme apontado por Martins (2013), para que diferentes atores participem efetivamente do processo de gestão estratégica, a Plataforma Ágora da Saúde deve estimular um fluxo contínuo de informações estratégicas, orientando suas ações de acordo com os princípios e diretrizes do SUS¹.
+        </p>
       </div>
 
       <div class="col-md-10"> 
         <p>
-          A Ágora possui duas frentes principais de atuação:
+          As duas frentes de atuação da Ágora:
+        </p>
+        <ol>
+          <li> 
+            Curadoria e Disseminação de Conteúdos: A Ágora promove a difusão de conteúdos sobre Educação, Ciência e Tecnologia Abertas no Brasil e no mundo. Isso é feito por meio de um blog e da área de eventos, onde você pode acessar textos, entrevistas, links para sites e ferramentas, vídeos, entre outros materiais relevantes. Assim, você não só aprenderá mais sobre o paradigma do conhecimento aberto, ainda pouco conhecido e disseminado, como também terá acesso a conteúdo que poderão ajudar a incorporar esses princípios em suas práticas diárias.
+          </li>
+          <li class="mt-3">
+            Oferta de Ferramentas de TI: A Ágora oferece ferramentas de tecnologia da informação para grupos interessados em construir interfaces na Internet que disponibilizem suas produções e informações, além de criar espaços de interação com pessoas externas. Os usuários podem criar Redes de Trabalho, públicas ou privadas, com conteúdo moderado ou não, decidindo quem pode visualizar e publicar na Rede. Essas Redes podem servir tanto para compartilhar informações amplamente, quanto para colaboração interna em projetos, atividades e produções do grupo.
+          </li>
+        </ol>
+        <p>
+          Criada em 2012 pela Fiocruz Brasília, a Ágora surgiu como um HUB virtual na área da saúde, atuando como um agregador e sistematizador das informações que circulam no ciberespaço, articuladas à produção diária do SUS. Desde então, o projeto evoluiu, incorporando novas configurações e redes específicas, como a Rede Saúde e Cultura, a Rede Serrana e a Rede Malária, entre outras.
         </p>
         <p>
-          Curadoria e disseminação de conteúdos sobre a Educação, Ciência e Tecnologia Abertas no Brasil e no Mundo;
+          Com base na experiência acumulada entre 2012 e 2017, e no mapeamento das demandas da Fiocruz e seus parceiros, o projeto foi reformulado para assumir sua nova configuração atual. Agora, a Ágora busca se consolidar como uma referência nos temas de Educação, Ciência e Tecnologia Abertas na área da saúde no Brasil.
         </p>
         <p>
-          Oferta de ferramentas de tecnologia da informação para grupos interessados em construir uma interface na Internet que cumpra o objetivo de disponibilizar informações e as produções do grupo, além de gerar um espaço de interação com pessoas externas.
+          Nesse novo desenho conceitual, a Ágora também fortalece sua interlocução com a Estratégia Fiocruz para a Agenda 2030, posicionando-se como um espaço virtual onde convergem ideias, iniciativas e projetos relacionados à Saúde, Ciência e Tecnologia, em alinhamento com os Objetivos de Desenvolvimento Sustentável da Agenda 2030. Assim, a plataforma se torna um espaço de promoção, mobilização e parceria, utilizando as funcionalidades oferecidas para alcançar esses objetivos.
         </p>
-        <p>
-          A primeira frente de trabalho é operacionalizada por meio do blog e da área de eventos da Ágora. Nele você poderá encontrar textos, entrevistas, links para sites e ferramentas, vídeos, dentre outros conteúdos relevantes sobre o tema. Dessa forma, além de conhecer mais sobre esse paradigma ainda pouco conhecido e disseminado do conhecimento aberto, você também terá a oportunidade de encontrar conteúdos que te ajudarão a desenvolver a abertura nas suas práticas de trabalho.
-        </p>
-        <p>
-          Na segunda frente, a Ágora possibilita que seus usuários criem Redes de trabalho. As Redes podem ser públicas ou secretas, de conteúdo moderado ou não. O poder está em suas mãos! Com isso, você poderá definir quem pode visualizar a Rede e publicar nela, permitindo criar uma interface para disponibilização de informações para qualquer pessoa, como eventos, blog, acervo de arquivos, dentre outros. Ou você pode criar uma Rede com o objetivo de colaboração entre os participantes do seu grupo, onde poderão ser sistematizadas informações sobre seus projetos, atividades e produções.
-        </p>
+        <p class="mb-3 fw-bold">E então, vamos experimentar?</p>
       </div>
 
-      <div class="col-md-10">
-        <h4 class="mb-3 mt-5">A Ágora e a Estratégia Fiocruz para a Agenda 2030</h4>
-        <p>
-          A Ágora surgiu em 2012 por iniciativa da Fundação Oswaldo Cruz Brasília, tendo como objetivo ser um HUB virtual na área da saúde, ou seja, um agregador e sistematizador das informações que fluem no ciberespaço de forma articulada à produção cotidiana  do SUS. Nesse tempo de existência o projeto passou por diferentes configurações, a começar pela constituição de redes e plataformas específicas, como as Redes Saúde e Cultura, Rede Serrana, Rede Malária, dentre outras.
-        </p>
-        <p>
-          A experiência acumulada entre 2012 e 2017 e o mapeamento de demandas na Fiocruz e parceiros apontaram para a necessidade de reformular o projeto dando-lhe essa nova configuração. Com o novo desenho conceitual e técnico,  a Ágora pretende se estabelecer como referência nos temas da Educação, Ciência e Tecnologias Abertas na área da saúde no Brasil.
-        </p>
-        <p>
-          Neste redesenho conceitual, a Ágora traz ainda uma forte interlocução com a Estratégia Fiocruz para a Agenda 2030. Propondo-se ser o espaço virtual onde se confluem-se ideias, iniciativas e projetos relacionados ao tema da Saúde, Ciência e Tecnologia que atendam aos Objetivos do Desenvolvimento Sustentável da Agenda 2030. Sendo assim, um espaço de promoção, mobilização e parceria através das funcionalidades que a plataforma oferece.
-        </p>
-        <p class="mb-5">E então, vamos experimentar?</p>
-      </div>
-
-      <div class="col-md-10">
-        <p class="text-bold">Créditos</p>
-          <div>
-            <p class="mb-1">Paulo Gadelha</p>
-            <p class="text-italic mb-4">Coordenador da Estratégia Fiocruz para Agenda 2030</p>
-          </div>
-          
-          <div>  
-            <p class="mb-1">Cláudia de Souza Ferreira Martins</p>
-            <p class="mb-1">Luiz Augusto Galvão</p>
-            <p class="mb-4"> Vinicius Ameixa</p>
-          </div>
-          
-          <div>
-            <p class="mb-1">Wagner de Jesus Martins</p>
-            <p class="text-italic mb-4">Coordenador da Plataforma Ágora</p>
-          </div>
-          
-          <div>
-            <p class="mb-1">Fernanda Machiner</p>
-            <p class="mb-1">Tereza Helena Gabrielli B. Campello</p>
-            <p class="mb-5">Pedro Henrique Silva Conde Aguiar</p>
-          </div>
-
-          <p class="text-bold">Equipe de Desenvolvimento</p>
-          
-          <div>
-            <p class="mb-1">Assunção Jr.</p>
-            <p class="mb-1">Augusto Yamamoto</p>
-            <p class="mb-1">David Veloso</p>
-            <p class="mb-1">Felipe Cavalcanti</p>
-            <p class="mb-1">João Ximenes</p>
-            <p class="mb-1">Juliana Vargas</p>
-            <p class="mb-1">Marcelo Soares Souza</p>
-            <p class="mb-1">Marcos Botelho</p>
-            <p class="mb-1">Marcus Vinicius de Barros Pontes</p>
-            <p class="mb-5">Thiago Petra</p>
-          </div>
+      <div class="col-md-10 text-center mb-3">
+        <p>Wagner de Jesus Martins</p>
+        <p>Coordenador da Plataforma Ágora</p>
       </div>
 
       <div class="col-md-10">
