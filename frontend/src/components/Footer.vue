719dc61a32968f015a1fa8ae527599c6c1197366
barbarabea <barbarabeatrizsilva6@gmail.com>
2024-09-03 15:13:05 -0300

    Responsividade do rodapé com classes bootstrap

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
 
     <div class="row justify-content-center py-5" style="background-image: linear-gradient(360deg, #575860 0%, #3a3a3a 100%);">
-        <div class="col-md-10 d-flex justify-content-between align-items-start pb-5">
-            <div class="col-3 py-4 px-4 align-items-start">
+        <div class="col-md-10 d-flex flex-wrap justify-content-between align-items-start pb-5">
+            <div class="col-10 col-md-3 py-4 px-4 align-items-start">
                 <div>
                     <p class="h5" style="color: #FFFFFF; font-size: 15px; font-weight: 600;">
                         <strong>SOBRE O ÁGORA</strong>
@@ -31,7 +31,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-2 py-4 px-2 align-items-start">
+            <div class="col-10 col-md-2 py-4 px-2 align-items-start">
                 <div>
                     <p class="h5" style="color: #FFFFFF; font-size: 15px; font-weight: 600;"><strong>MAPA DO SITE</strong></p>
                     <p>
@@ -45,16 +45,16 @@
                     </p>
                 </div>
             </div>
-            <div class="col-7 py-4 px-2 align-items-start">
+            <div class="col-10 col-md-7 py-4 px-2 align-items-start">
                 <div>
                     <p class="h5" style="color: #FFFFFF; font-size: 15px; font-weight: bolder;">
                         REALIZAÇÃO
                     </p>
-                    <div class="d-flex align-items-center">
-                        <img src="@/assets/images/efas-rodape.png" alt="Logo estratégia Fiocruz Agenda 2030" class="px-2">
-                        <img src="@/assets/images/logo-fiocruz-rodape.png" alt="Imagem Fiocruz" class="px-2">
-                        <img src="@/assets/images/logo-ms-rodape.png" alt="Logo Ministério da Saúde" class="px-2">
-                        <img src="@/assets/images/logo-gov-rodape.png" alt="Logo Governo Federal" class="px-2">
+                    <div class="d-flex flex-wrap align-items-center">
+                        <img src="@/assets/images/efas-rodape.png" alt="Logo estratégia Fiocruz Agenda 2030" class="px-2 pt-2">
+                        <img src="@/assets/images/logo-fiocruz-rodape.png" alt="Imagem Fiocruz" class="px-2 pt-2">
+                        <img src="@/assets/images/logo-ms-rodape.png" alt="Logo Ministério da Saúde" class="px-2 pt-2">
+                        <img src="@/assets/images/logo-gov-rodape.png" alt="Logo Governo Federal" class="px-2 pt-2">
                     </div>
                 </div>
             </div>
