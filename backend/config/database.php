687f497eeaa52ba4d46a6c6cf2b13390fe3d66a4
Alylson Monteiro <alylsonmonteiro@lampp-it.com.br>
2024-09-11 11:28:09 -0300

    Retornando o public da configuração do postgres

--- backend/config/database.php
+++ backend/config/database.php
@@ -90,8 +90,8 @@ return [
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            'schema'   => env('DB_SCHEMA', 'public'),
-            //'search_path' => 'public',
+            //'schema'   => env('DB_SCHEMA', 'public'),
+            'search_path' => 'public',
             'sslmode' => 'prefer',
         ],
 
