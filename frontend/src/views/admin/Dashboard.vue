b78db8488b62be454624f4bd937fa4433052dd22
Frinhani <thiago.frinhani@ths.inf.br>
2024-09-12 16:52:07 -0300

    Alterando arquivo admin

--- frontend/src/views/admin/Dashboard.vue
+++ frontend/src/views/admin/Dashboard.vue
@@ -1,52 +1,20 @@
 <template>
-  <div class="dashboard-wrapper" :class="{ 'collapsed': isSidebarCollapsed }">
-    <Sidebar :is-collapsed="isSidebarCollapsed" @toggle-sidebar="toggleSidebar"/>
-    <div class="dashboard-content">
-      <Navbar  @toggle-sidebar="toggleSidebar"/>
-      <main>
-        <router-view />
-      </main>
+  <div class="container-fluid">
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item active" aria-current="page"><span class="text-black-50">Home</span></li>
+      </ol>
+    </nav>
+    <div class="card w-100" style="background-color: #4CAF50; height: 6% !important;">
+      <div class="card-body text-white">
+        Você está logado!
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Sidebar from '@/components/admin/Sidebar.vue';
-import Navbar from '@/components/Navbar.vue';
-
 export default {
-  components: {
-    Sidebar,
-    Navbar
-  },
-  data() {
-    return {
-      isSidebarCollapsed: false,
-    };
-  },
-  methods: {
-    toggleSidebar() {
-      this.isSidebarCollapsed = !this.isSidebarCollapsed;
-    },
-  }
+  name: 'Dashboard'
 };
 </script>
-
-<style scoped>
-.dashboard-wrapper {
-  display: flex;
-  height: 100vh;
-}
-
-.dashboard-content {
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-}
-
-main {
-  padding: 20px;
-  background-color: #eee;
-  flex: 1;
-}
-</style>
